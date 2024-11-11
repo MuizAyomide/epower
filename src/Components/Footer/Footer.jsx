e870735b5cc87d1ad39ee5b1303010fa6a3862da
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-11-11 22:07:16 +0100

    commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="left">
+            <h1>
+            Harnessing the  power of AI and no-code/low-code technologies
+            </h1>
+        </div>
+        <div className="right">
+            <div className="first">
+                <ul>
+                    <li>Home</li>
+                    <li>What we do</li>
+                    <li>Our works</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className="first">
+                <ul>
+                    <li>Behance</li>
+                    <li>Dribble</li>
+                    <li>LinkedIn</li>
+                    <li>Instagram</li>
+                </ul>
+            </div>
+        </div>
+
+        <p>© 2024 EpowerNG. All rights reserved.</p>
+      
+    </div>
+  )
+}
+
+export default Footer
