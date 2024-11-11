e937b25cf2e87c982e154101ca40075af28cd4b6
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-11-11 22:07:53 +0100

    commit

---
+++ src/Components/Newsletter/Newletter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Newsletter.css";
+
+const Newletter = () => {
+  return (
+    <div className="newsletter container section">
+        <div className="letter">
+
+      <h1>Transform your business</h1>
+      <p>
+        Connect with Epower to leverage the potential of tailored AI and no-code
+        solutions that not only protect but propel your business into the
+        future.
+      </p>
+      <button className="btn btn3">Get Started today</button>
+        </div>
+    </div>
+  );
+};
+
+export default Newletter;
