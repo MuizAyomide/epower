d4198784c92abd75226fc71b58d447df1f319df2
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-11-12 09:34:21 +0100

    commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -16,6 +16,13 @@ const Navbar = () => {
           <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
         </svg>
 
+        <div className="desk_bar">
+          <div className="list">What we do</div>
+          <div className="list">Our works</div>
+          <div className="list">Contact</div>
+          <button className="list btn left">Get Started</button>
+        </div>
+
       </div>
     </div>
   );
