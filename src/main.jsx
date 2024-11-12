2f04ed65e8a862e83194766ac997c13df9a38bd5
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-11-12 23:37:12 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import './Styles/utils.css'
+import './Styles/Utils.css'
 import './Styles/webflow.css'
 import './Styles/Modern-Normalize.css'
 
