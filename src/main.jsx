f9a8bb141882045c08a7ec17c4224b664b424b12
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-11-12 23:41:47 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import './Styles/Utils.css'
-import './Styles/webflow.css'
+import './Styles/WebFlow.css'
 import './Styles/Modern-Normalize.css'
 
 createRoot(document.getElementById('root')).render(
