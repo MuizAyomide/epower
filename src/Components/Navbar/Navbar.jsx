b51716673debd1c9180aaaee7a33d9712aac67fa
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-11-15 23:20:51 +0100

    commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Navbar.css";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -7,34 +8,48 @@ const Navbar = () => {
   return (
     <div className="navbar container">
       <div className="nav">
-        {
-          open === false ? 
-          <img className="logo" src="/image 2.webp" alt="" />:
+        {open === false ? (
+          <img className="logo" src="/image 2.webp" alt="" />
+        ) : (
           <img className="big_logo" src="/image 2.webp" alt="" />
-        }
+        )}
         {open === true ? (
           <div className="mobile_bar">
             <div className="mobile_nav">
-
-            <img className="nav_logo logo" src="/image 2.webp" alt="" />
-            <div className="close_menu menu" onClick={() => setOpen(!open)}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-                fill="#000"
+              <img className="nav_logo logo" src="/image 2.webp" alt="" />
+              <div className="close_menu menu" onClick={() => setOpen(!open)}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24px"
+                  viewBox="0 -960 960 960"
+                  width="24px"
+                  fill="#000"
                 >
-                <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
-              </svg>
-                </div>
+                  <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+                </svg>
+              </div>
             </div>
             {/* <Link to="about" className="navbar-link">
             About
           </Link> */}
-            <div className="list">What we do</div>
-            <div className="list">Our works</div>
-            <div className="list">Contact</div>
+            <Link 
+          smooth={true}
+          duration={500}
+          to="offer" className="list">
+            What we do
+          </Link>
+          <Link
+          smooth={true}
+          duration={500}
+          to="work" className="list">
+            Our works
+          </Link>
+          <Link 
+          smooth={true}
+          duration={500}
+          to="contact" className="list">
+            Contact
+          </Link>
             <button className="list btn left">Get Started</button>
           </div>
         ) : (
@@ -50,9 +65,24 @@ const Navbar = () => {
         )}
 
         <div className="desk_bar">
-          <div className="list">What we do</div>
-          <div className="list">Our works</div>
-          <div className="list">Contact</div>
+          <Link 
+          smooth={true}
+          duration={500}
+          to="offer" className="list">
+            What we do
+          </Link>
+          <Link
+          smooth={true}
+          duration={500}
+          to="work" className="list">
+            Our works
+          </Link>
+          <Link 
+          smooth={true}
+          duration={500}
+          to="contact" className="list">
+            Contact
+          </Link>
           <button className="list btn left">Get Started</button>
         </div>
       </div>
