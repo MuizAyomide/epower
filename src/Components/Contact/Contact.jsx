71b622c9e14eafc77207e2c439f2c7952e9a3713
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-11-18 18:38:06 +0100

    commit

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,34 +1,68 @@
 import React from "react";
 import "./Contact.css";
+import { useEffect } from "react";
 
 const Contact = () => {
+
+  useEffect(() => {
+    // Observer for write-up elements
+    const writeUpObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show_text'); 
+            }
+        });
+    });
+
+    // Observer for image elements
+    const imageObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show_boxs'); 
+            }
+        });
+    });
+
+    const animatedElements = document.querySelectorAll('.text_animation');
+    const animatedImages = document.querySelectorAll('.boxs_animation');
+
+    animatedElements.forEach((el) => writeUpObserver.observe(el));
+    animatedImages.forEach((image) => imageObserver.observe(image));
+
+    // Cleanup
+    return () => {
+        animatedElements.forEach((el) => writeUpObserver.unobserve(el));
+        animatedImages.forEach((image) => imageObserver.unobserve(image));
+    };
+}, []);
+
   return (
     <div className="contact section container">
-      <h1>Contact us</h1>
-      <p>
+      <h1 className="text_animation">Contact us</h1>
+      <p className="text_animation">
         Seen enough, we’d love to leverage our expertise to help you too. Get in
         touch with us with the details below.
       </p>
       <div className="contact_boxs">
-        <div className="contact_box">
+        <div className="contact_box boxs_animation">
           <img src="email-icon-fill.svg" alt="" />
-          <h3>Email</h3>
-          <p>info@epower.ng</p>
+          <h3 className="text_animation">Email</h3>
+          <p className="text_animation">info@epower.ng</p>
         </div>
-        <div className="contact_box">
+        <div className="contact_box boxs_animation">
           <img src="phone-icon.svg" alt="" />
-          <h3>Phone</h3>
-          <p>08037285780</p>
+          <h3 className="text_animation">Phone</h3>
+          <p className="text_animation">08037285780</p>
         </div>
-        <div className="contact_box">
+        <div className="contact_box boxs_animation">
           <img src="pin-icon.svg" alt="" />
-          <h3>Office</h3>
-          <p>
+          <h3 className="text_animation">Office</h3>
+          <p className="text_animation">
             Topmost floor, Alhaja T.A Lanihun Shopping Complex, Bashorun,
             Ibadan, Oyo State, Nigeria.
           </p>
 
-          <p><u>View on Map</u></p>
+          <p className="text_animation"><u>View on Map</u></p>
         </div>
       </div>
     </div>
