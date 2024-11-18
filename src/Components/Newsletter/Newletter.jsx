81b051027d84cb94a986154bf1c204c530cb66a5
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-11-18 18:00:57 +0100

    commit

--- src/Components/Newsletter/Newletter.jsx
+++ src/Components/Newsletter/Newletter.jsx
@@ -1,21 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Newsletter.css";
+// import './Observer.js'; // Ensure this file is correctly set up
 
-const Newletter = () => {
-  return (
-    <div className="newsletter container section">
-        <div className="letter">
+const Newsletter = () => {
+    
 
-      <h1>Transform your business</h1>
-      <p>
-        Connect with Epower to leverage the potential of tailored AI and no-code
-        solutions that not only protect but propel your business into the
-        future.
-      </p>
-      <button className="btn btn3">Get Started today</button>
+    return (
+        <div className="newsletter container section hidden">
+            <div className="letter">
+                <h1>Transform your business</h1>
+                <p>
+                    Connect with Epower to leverage the potential of tailored AI and no-code
+                    solutions that not only protect but propel your business into the
+                    future.
+                </p>
+                <button className="btn btn3">Get Started today</button>
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
-export default Newletter;
+export default Newsletter;
